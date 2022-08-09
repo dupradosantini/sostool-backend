8e1ecc064f4d2094ea75678825b026340715e8f6
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-08-09 12:04:07 -0300

    Initial commit

---
+++ src/main/java/dupradosantini/sostoolbackend/SostoolBackendApplication.java
@@ -0,0 +1,13 @@
+package dupradosantini.sostoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SostoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SostoolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dupradosantini/sostoolbackend/SostoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dupradosantini.sostoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SostoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
